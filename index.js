1b4d93bdb9c41a17b3fd57bc025143351729253b
williamdinh <hoangdinh2411@gmail.com>
2023-05-15 09:53:16 +0200

    Configuration and  structur project

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const app = express();
-const port = process.env.port || 8000;
+const express = require('express')
+const { json } = require('express')
+const app = express()
+app.use(express.json())
+app.use('/api', require('./api/routers/index.js'))
+app.use(json())
 
+const port = process.env.PORT || 8000
 app.listen(port, () => {
-    console.log("Server startad");
+  console.log('The server listening on port ' + port)
 })
