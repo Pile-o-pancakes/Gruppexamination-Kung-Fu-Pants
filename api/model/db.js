285b7205fb871de95cef67cb522151c085757b09
williamdinh <hoangdinh2411@gmail.com>
2023-05-15 15:51:48 +0200

    pull new version

--- api/model/db.js
+++ api/model/db.js
@@ -1,1 +1,17 @@
 const sqlite3 = require('sqlite3').verbose()
+
+const db = new sqlite3.Database('./db/Chinook_Sqlite.sqlite')
+
+const connectDatabase = () => {
+  const db = new sqlite3.Database('./db/Chinook_Sqlite.sqlite')
+  db.serialize(() => {
+    db.each(
+      'SELECT name FROM sqlite_master WHERE type="table"',
+      (err, table) => {
+        console.log(table)
+      }
+    )
+  })
+
+  return db
+}
