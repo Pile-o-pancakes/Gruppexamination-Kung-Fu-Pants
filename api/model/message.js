8b862c2bda41e8bb715a7c87269a600f7b6db3f9
williamdinh <hoangdinh2411@gmail.com>
2023-05-18 12:29:00 +0200

    get list of  members by group Id

--- api/model/message.js
+++ api/model/message.js
@@ -4,7 +4,7 @@ const MessageTable = {
   getMessages: (group_id, sort) => {
     return new Promise((resolve, reject) => {
       db.all(
-        `SELECT *, User.Username FROM Message AS M JOIN User ON User.Id = M.UserId WHERE M.GroupId = ? ORDER BY M.CreatedAt ${sort};`,
+        `SELECT M.Id, M.Content, M.CreatedAt, ,User.Username FROM Message AS M JOIN User ON User.Id = M.UserId WHERE M.GroupId = ? ORDER BY M.CreatedAt ${sort};`,
         [group_id],
         (err, row) => {
           if (err) {
